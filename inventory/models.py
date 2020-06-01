0677ba39ddab2c267d54c8ef63e380d51c4b5cea
unknown <pemeraldy@gmail.com>
2020-06-01 12:38:26 +0100

    setup sqlite file for database migrations

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Device(models.Model): #table's name
+    #names of columns
+    type = models.CharField(max_length=100, blank=False)
+    price = models.IntegerField()
+
+    choices = (
+        ('AVAILABLE', 'Item available for purchase'),
+        ('SOLD', 'Item not currently available'),
+        ('RESTOCKING', 'Item will be availble in few days - restocking')
+    )
+    status = models.CharField(max_length=10, choices = choices, default="SOLD")
+    issues = models.CharField(max_length=100, default="No issues")
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return 'Type: {0} Price: {1}'.format(self.type, self.price)
+
+class Laptop(Device):
+    pass
+
+class Desktop(Device):
+    pass
+
+class Mobile(Device):
+    pass
